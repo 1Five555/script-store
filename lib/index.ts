408d6529aaacf6c6e81c2928d284d077309c0880
14k <994871915@qq.com>
2025-01-16 00:30:36 +0800

    feat:header && footer

---
+++ lib/index.ts
@@ -0,0 +1,8 @@
+export const META_DATA = {
+  APP_NAME: process.env["NEXT_PUBLIC_APP_NAME"] || "Script store",
+  APP_DESCRIPTION:
+    process.env["NEXT_PUBLIC_APP_DESCRIPTION"] || "A script store",
+};
+
+export const SERVER_URL =
+  process.env["NEXT_PUBLIC_SERVER_URL"] || "http://localhost:3000";
